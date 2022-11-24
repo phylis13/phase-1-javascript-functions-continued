5b7d33a31223e1e8b7fb6d74f1dc4133ec34b57b
phylis13 <phylisnjeri54@gmail.com>
2022-11-24 23:25:43 +0300

    comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(action="roller-skate") {
+    return `This Saturday, I want to ${action}!`
+  }
+  
+  let mondayWork = function(action="go to the office") {
+    return `This Monday, I will ${action}.`
+  }
+  
+  let wrapAdjective = function(symbol="*") {
+    return function(adjective="special") {
+      return `You are ${symbol}${adjective}${symbol}!`
+    }
+}
